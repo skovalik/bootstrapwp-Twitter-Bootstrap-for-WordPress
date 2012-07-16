925ddcc330da6dcb99dfe55684ec2c4bcdf754b1
Rachel Baker <rachel@rachelbaker.me>
2012-07-16 11:24:53 -0500

    Removed header element wrapping around page title

--- page.php
+++ page.php
@@ -8,31 +8,26 @@
  * @package WordPress
  * @subpackage WP-Bootstrap
  * @since WP-Bootstrap 0.1
+ *
+ * Last Revised: July 16, 2012
  */
 
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <div class="row">
   <div class="container">
-   <?php if (function_exists('bootstrapwp_breadcrumbs')) bootstrapwp_breadcrumbs(); ?>
+   <?php if ( function_exists( 'bootstrapwp_breadcrumbs' ) ) bootstrapwp_breadcrumbs(); ?>
    </div><!--/.container -->
    </div><!--/.row -->
    <div class="container">
-
-      
- <!-- Masthead
-      ================================================== -->
-      <header class="jumbotron subhead" id="overview">
         <h1><?php the_title();?></h1>
-      </header>
-         
         <div class="row content">
 <div class="span8">
 
             <?php the_content();?>
 <?php endwhile; // end of the loop. ?>
           </div><!-- /.span8 -->
-          
+
           <?php get_sidebar(); ?>
 
 
