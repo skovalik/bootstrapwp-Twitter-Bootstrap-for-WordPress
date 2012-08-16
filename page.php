49e1460a54f8ba6b42097a6394dd80b0e8962bff
Rachel Baker <rachel@rachelbaker.me>
2012-08-15 21:43:20 -0500

    Cleaned up header tags in page.php and single.php

--- page.php
+++ page.php
@@ -20,7 +20,9 @@ get_header(); ?>
    </div><!--/.container -->
    </div><!--/.row -->
    <div class="container">
+    <header class="page-title">
         <h1><?php the_title();?></h1>
+      </header>
         <div class="row content">
 <div class="span8">
 
