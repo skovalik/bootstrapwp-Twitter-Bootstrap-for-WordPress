d7b77c3586f2795f9f4e312b7cd35a50a68e340c
Rachel Baker <rachel@rachelbaker.me>
2012-11-10 22:46:07 -0600

    Using get_template_directory_uri() instead of bloginfo('template_url')

--- header.php
+++ header.php
@@ -39,11 +39,11 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
   <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="<?php bloginfo( 'template_url' );?>/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo( 'template_url' );?>/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo( 'template_url' );?>/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo( 'template_url' );?>/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="<?php bloginfo( 'template_url' );?>/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="<?php get_template_directory_uri();?>/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php get_template_directory_uri();?>/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php get_template_directory_uri();?>/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php get_template_directory_uri();?>/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php get_template_directory_uri();?>/ico/apple-touch-icon-57-precomposed.png">
 
   <!--[if lt IE 9]>
 <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
