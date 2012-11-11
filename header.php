0623b09d1cefa269de6f0727737e9cbbfee448a3
Rachel Baker <rachel@rachelbaker.me>
2012-11-11 12:54:25 -0600

    Removed wp_title logic for a leaner header file

--- header.php
+++ header.php
@@ -7,59 +7,43 @@
  * @subpackage Default_Theme
  * @since WP-Bootstrap 0.1
  *
- * Last Revised: August 15, 2012
  */ ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
-   <title><?php
-  /*
-   * Print the <title> tag based on what is being viewed.
-   */
-  global $page, $paged;
 
-  wp_title( '|', true, 'right' );
-
-  // Add the blog name.
-  bloginfo( 'name' );
-
-  // Add the blog description for the home/front page.
-  $site_description = get_bloginfo( 'description', 'display' );
-  if ( $site_description && ( is_home() || is_front_page() ) )
-    echo " | $site_description";
-
-  // Add a page number if necessary:
-  if ( $paged >= 2 || $page >= 2 )
-    echo ' | ' . sprintf( __( 'Page %s', 'bootstrapwp' ), max( $paged, $page ) );
-
-  ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-    <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?php get_template_directory_uri();?>/ico/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri();?>/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri();?>/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri();?>/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri();?>/ico/apple-touch-icon-57-precomposed.png">
+
     <?php wp_head(); ?>
+
   </head>
   <body <?php body_class(); ?>  data-spy="scroll" data-target=".bs-docs-sidebar" data-offset="10">
     <div class="navbar navbar-inverse navbar-relative-top">
-           <div class="navbar-inner">
-             <div class="container">
-           <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-          </button>
+          </a>
           <a class="brand" href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
           <?php
            /** Loading WordPress Custom Menu  **/
            wp_nav_menu( array(
               'menu'            => 'main-menu',
-              'container_class' => 'nav-collapse',
+              'container_class' => 'nav-collapse collapse',
               'menu_class'      => 'nav',
               'fallback_cb'     => '',
               'menu_id' => 'main-menu',
