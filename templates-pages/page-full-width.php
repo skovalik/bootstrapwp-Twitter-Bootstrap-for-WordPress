8c9015a5536073b609922bc4c377a94a1dfd1a3d
Rachel Baker <rachel@rachelbaker.me>
2012-11-14 13:17:06 -0600

    Removed "Jumbotron" class from page title.  Added missing /div for .container. Cleaned up template alignment.

--- templates-pages/page-full-width.php
+++ templates-pages/page-full-width.php
@@ -10,23 +10,24 @@
 
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-    <div class="container">
-        <div class="row">
-          <div class="span12">
-             <?php if (function_exists('bootstrapwp_breadcrumbs')) bootstrapwp_breadcrumbs(); ?>
-         </div><!--/.span12 -->
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+       <?php if (function_exists('bootstrapwp_breadcrumbs')) bootstrapwp_breadcrumbs(); ?>
+     </div><!--/.span12 -->
    </div><!--/.row -->
 
 
-      <header class="jumbotron subhead" id="overview">
-        <h1><?php the_title();?></h1>
-      </header>
+   <header class="subhead" id="overview">
+    <h1><?php the_title();?></h1>
+  </header>
 
-				<div class="row content">
-				  <?php the_content();?>
-				<?php endwhile; // end of the loop. ?>
+  <div class="row content">
+    <?php the_content();?>
+  <?php endwhile; // end of the loop. ?>
 
-				</div><!-- .row content -->
+</div><!-- .row content -->
+</div><!--/.container -->
 
 
 <?php get_footer(); ?>
