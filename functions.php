5d2f253883d867a891cbdb6820c6cf4d64dd3281
Rachel Baker <rachel@rachelbaker.me>
2012-11-14 13:27:33 -0600

    Added bootstrap_wp_title function

--- functions.php
+++ functions.php
@@ -401,7 +401,33 @@ function bootstrapwp_autoset_featured_img() {
   }
 }  //end function
 
+/*
+| -------------------------------------------------------------------
+| Formatting Page Title Elements
+| -------------------------------------------------------------------
+| Conditionally determines default title to display.
+| */
+function bootstrapwp_wp_title( $title, $sep ) {
+  global $paged, $page;
 
+  if ( is_feed() )
+    return $title;
+
+  // Add the site name.
+  $title .= get_bloginfo( 'name' );
+
+  // Add the site description for the home/front page.
+  $site_description = get_bloginfo( 'description', 'display' );
+  if ( $site_description && ( is_home() || is_front_page() ) )
+    $title = "$title $sep $site_description";
+
+  // Add a page number if necessary.
+  if ( $paged >= 2 || $page >= 2 )
+    $title = "$title $sep " . sprintf( __( 'Page %s', 'bootstrapwp' ), max( $paged, $page ) );
+
+  return $title;
+}
+add_filter( 'wp_title', 'bootstrapwp_wp_title', 10, 2 );
 /*
 | -------------------------------------------------------------------
 | Adding Breadcrumbs
