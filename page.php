8cfad9da91ecf2a9d3722daf0c229864e9147b39
Rachel Baker <rachel@rachelbaker.me>
2012-11-14 13:32:59 -0600

    cleaned up formatting

--- page.php
+++ page.php
@@ -12,24 +12,24 @@
  */
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-      <div class="container">
-        <div class="row">
-          <div class="span12">
-             <?php if (function_exists('bootstrapwp_breadcrumbs')) bootstrapwp_breadcrumbs(); ?>
-         </div><!--/.span12 -->
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+       <?php if (function_exists('bootstrapwp_breadcrumbs')) bootstrapwp_breadcrumbs(); ?>
+     </div><!--/.span12 -->
    </div><!--/.row -->
 
-    <header class="page-title">
-        <h1><?php the_title();?></h1>
-      </header>
-        <div class="row content">
-<div class="span8">
+   <header class="page-title">
+    <h1><?php the_title();?></h1>
+  </header>
+  <div class="row content">
+    <div class="span8">
 
-            <?php the_content();?>
-<?php endwhile; // end of the loop. ?>
-          </div><!-- /.span8 -->
+      <?php the_content();?>
+      <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'bootstrapwp' ), 'after' => '</div>' ) ); ?>
+      <?php edit_post_link( __( 'Edit', 'bootstrapwp' ), '<span class="edit-link">', '</span>' ); ?>
+    <?php endwhile; // end of the loop. ?>
+  </div><!-- /.span8 -->
 
-          <?php get_sidebar(); ?>
-
-
-<?php get_footer(); ?>
+  <?php get_sidebar(); ?>
+  <?php get_footer(); ?>
