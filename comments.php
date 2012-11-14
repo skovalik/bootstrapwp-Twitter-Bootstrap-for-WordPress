d46eb18a55f8c6da60058d2a56f787642bc7fd20
Rachel Baker <rachel@rachelbaker.me>
2012-11-14 13:14:45 -0600

    Let's use the new Bootstrap Media list to display comments.  Changed comment list to a ul element and added the media-list class.

--- comments.php
+++ comments.php
@@ -10,55 +10,49 @@
  * @package WordPress
  * @subpackage WP-Bootstrap
  * @since WP-Bootstrap 0.1
- * 
- * Last Revised: February 4, 2012
+ *
  */
-?>
-	<div id="comments">
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'bootstrapwp' ); ?></p>
-	</div><!-- #comments -->
-	<?php
-			/* Stop the rest of comments.php from being processed,
-			 * but don't kill the script entirely -- we still have
-			 * to fully load the template.
-			 */
-			return;
-		endif;
-	?>
 
-	<?php // You can start editing here -- including this comment! ?>
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() )
+	return;
+?>
+<div id="comments" class="comments-area">
 
 	<?php if ( have_comments() ) : ?>
-		<h2 id="comments-title">
-			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'bootstrapwp' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+	<h2 id="comments-title">
+		<?php
+		printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'bootstrapwp' ),
+		       number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+		       ?>
+		   </h2>
 
-		<ol class="commentlist">
-			<?php
-				/* Loop through and list the comments. Tell wp_list_comments()
-				 * to use bootstrapwp_comment() to format the comments.
-				 * If you want to overload this in a child theme then you can
-				 * define bootstrapwp_comment() and that will be used instead.
-				 * See bootstrapwp_comment() in bootstrapwp/functions.php for more.
-				 */
-				wp_list_comments( array( 'callback' => 'bootstrapwp_comment' ) );
-			?>
-		</ol>
+		   <ul class="commentlist media-list">
+		   	<?php
+		   	wp_list_comments( array( 'callback' => 'bootstrapwp_comment' ) );
+		   	?>
+		   </ul><!-- /.commentlist -->
+		   <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		   <nav id="comment-nav-below" class="navigation" role="navigation">
+		   	<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'minimallybaked' ); ?></h1>
+		   	<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'minimallybaked' ) ); ?></div>
+		   	<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'minimallybaked' ) ); ?></div>
+		   </nav>
+		<?php endif; // check for comment navigation ?>
 
-
-	<?php
+		<?php
 		/* If there are no comments and comments are closed, let's leave a little note, shall we?
 		 * But we don't want the note on pages or post types that do not support comments.
 		 */
-		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
+		elseif ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+			?>
 		<p class="nocomments"><?php _e( 'Comments are closed.', 'bootstrapwp' ); ?></p>
 	<?php endif; ?>
 
 	<?php comment_form(); ?>
 
-</div><!-- #comments -->
+</div><!-- #comments .comments-area -->
