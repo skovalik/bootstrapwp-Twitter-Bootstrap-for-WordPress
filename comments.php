f7ba06ee5efa265915d3e67d59ee2ded981329ec
Rachel Baker <rachel@rachelbaker.me>
2012-11-26 19:29:31 -0600

    Let's clean-up some PHP formatting

--- comments.php
+++ comments.php
@@ -18,41 +18,55 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() )
-	return;
+if (post_password_required()) {
+    return;
+}
 ?>
 <div id="comments" class="comments-area">
 
-	<?php if ( have_comments() ) : ?>
-	<h2 id="comments-title">
-		<?php
-		printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'bootstrapwp' ),
-		       number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-		       ?>
-		   </h2>
+    <?php if (have_comments()) : ?>
+    <h2 id="comments-title">
+        <?php
+        printf(
+            _n(
+                'One thought on &ldquo;%2$s&rdquo;',
+                '%1$s thoughts on &ldquo;%2$s&rdquo;',
+                get_comments_number(),
+                'bootstrapwp'
+            ),
+            number_format_i18n(get_comments_number()),
+            '<span>' . get_the_title() . '</span>'
+        );
+        ?>
+    </h2>
 
-		   <ul class="commentlist media-list">
-		   	<?php
-		   	wp_list_comments( array( 'callback' => 'bootstrapwp_comment' ) );
-		   	?>
-		   </ul><!-- /.commentlist -->
-		   <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		   <nav id="comment-nav-below" class="navigation" role="navigation">
-		   	<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'minimallybaked' ); ?></h1>
-		   	<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'minimallybaked' ) ); ?></div>
-		   	<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'minimallybaked' ) ); ?></div>
-		   </nav>
-		<?php endif; // check for comment navigation ?>
+    <ul class="commentlist media-list">
+        <?php
+        wp_list_comments(array('callback' => 'bootstrapwp_comment'));
+        ?>
+    </ul><!-- /.commentlist -->
+    <?php if (get_comment_pages_count() > 1 && get_option(
+        'page_comments'
+    )
+    ) : // are there comments to navigate through ?>
+        <nav id="comment-nav-below" class="navigation" role="navigation">
+            <h1 class="assistive-text section-heading"><?php _e('Comment navigation', 'minimallybaked'); ?></h1>
 
-		<?php
-		/* If there are no comments and comments are closed, let's leave a little note, shall we?
+            <div class="nav-previous"><?php previous_comments_link(
+                __('&larr; Older Comments', 'minimallybaked')
+            ); ?></div>
+            <div class="nav-next"><?php next_comments_link(__('Newer Comments &rarr;', 'minimallybaked')); ?></div>
+        </nav>
+        <?php endif; // check for comment navigation ?>
+
+    <?php /* If there are no comments and comments are closed, let's leave a little note, shall we?
 		 * But we don't want the note on pages or post types that do not support comments.
 		 */
-		elseif ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-			?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'bootstrapwp' ); ?></p>
-	<?php endif; ?>
+elseif (!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type(), 'comments')) :
+    ?>
+    <p class="nocomments"><?php _e('Comments are closed.', 'bootstrapwp'); ?></p>
+    <?php endif; ?>
 
-	<?php comment_form(); ?>
+    <?php comment_form(); ?>
 
 </div><!-- #comments .comments-area -->
