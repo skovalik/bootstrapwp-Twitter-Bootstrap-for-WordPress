6d7fb6c8e25bec103d4b4d5f9f118baac448dd0e
Rachel Baker <rachel@rachelbaker.me>
2013-01-13 21:27:56 -0600

    Cleaned up formatting on blog page and sidebar

--- sidebar.php
+++ sidebar.php
@@ -6,15 +6,15 @@
  * @subpackage BootstrapWP
  */
 ?>
-<div class="span4">
-    <div class="well sidebar-nav">
-        <?php
-        if (function_exists('dynamic_sidebar')) {
-            dynamic_sidebar("sidebar-page");
-        }
-        ?>
-    </div>
-    <!--/.well .sidebar-nav -->
-</div><!-- /.span4 -->
-</div><!-- /.row .content -->
+        <div class="span4">
+            <div class="well sidebar-nav">
+                <?php
+                if (function_exists('dynamic_sidebar')) {
+                    dynamic_sidebar("sidebar-page");
+                }
+                ?>
+            </div>
+            <!--/.well .sidebar-nav -->
+        </div><!-- /.span4 -->
+    </div><!-- /.row .content -->
 </div><!--/.container -->
--- templates-pages/page-blog.php
+++ templates-pages/page-blog.php
@@ -9,57 +9,53 @@
 get_header(); ?>
 <?php while (have_posts()) : the_post(); ?>
 
-  <div class="container">
-    <div class="row">
-        <div class="span12">
-            <?php if (function_exists('bootstrapwp_breadcrumbs')) {
-            bootstrapwp_breadcrumbs();
-        } ?>
-        </div>
-        <!--/.span12 -->
-    </div><!--/.row -->
-
-    <header class="subhead" id="overview">
-        <h1><?php the_title();?></h1>
-    </header>
-
-  <div class="row content">
-    <div class="span8">
-      <?php the_content();
-endwhile;
-// end of the loop
-wp_reset_query();
-// resetting the loop
-?>
-    <hr/>
-
-<?php
-// Blog post query
-$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-query_posts(array('post_type' => 'post', 'paged' => $paged, 'showposts' => 0));
-if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div <?php post_class(); ?>>
-        <a href="<?php the_permalink(); ?>" title="<?php the_title();?>"><h3><?php the_title();?></h3></a>
-
-        <p class="meta"><?php echo bootstrapwp_posted_on();?></p>
-
+    <div class="container">
         <div class="row">
-            <div class="span2">
-                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                    <?php echo bootstrapwp_autoset_featured_img(); ?></a>
-            </div>
-            <!-- /.span2 -->
-            <div class="span6">
-                <?php the_excerpt();?>
-            </div>
-            <!-- /.span6 -->
-        </div>
-        <!-- /.row -->
-        <hr/>
-    </div><!-- /.post_class -->
-    <?php endwhile; endif; ?>
-<?php bootstrapwp_content_nav('nav-below'); ?>
+            <div class="span12">
+                <?php if (function_exists('bootstrapwp_breadcrumbs')) {
+                    bootstrapwp_breadcrumbs();
+                } ?>
+            </div><!--/.span12 -->
+        </div><!--/.row -->
 
-   </div><!-- /.span8 -->
-<?php get_sidebar('blog'); ?>
-<?php get_footer(); ?>
+        <header class="page-title">
+            <h1><?php the_title();?></h1>
+        </header>
+
+      <div class="row content">
+        <div class="span8">
+            <?php the_content(); ?>
+            <?php endwhile;
+                // reset the loop
+                wp_reset_query(); ?>
+            <hr/>
+
+            <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            // Blog post query
+            query_posts(array('post_type' => 'post', 'paged' => $paged, 'showposts' => 0));
+            if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div <?php post_class(); ?>>
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title();?>">
+                        <h3><?php the_title();?></h3></a>
+                    <p class="meta">
+                        <?php echo bootstrapwp_posted_on();?>
+                    </p>
+
+                    <div class="row">
+                        <div class="span2">
+                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                <?php echo bootstrapwp_autoset_featured_img(); ?></a>
+                        </div><!-- /.span2 -->
+                        <div class="span6">
+                            <?php the_excerpt();?>
+                        </div><!-- /.span6 -->
+                    </div><!-- /.row -->
+                    <hr/>
+                </div><!-- /.post_class -->
+            <?php endwhile; endif; ?>
+                <?php bootstrapwp_content_nav('nav-below'); ?>
+        </div><!-- /.span8 -->
+
+    <?php get_sidebar('blog'); ?>
+    <?php get_footer(); ?>
