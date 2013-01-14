748605b393fd27098dd22572434e04939f78eaf6
Rachel Baker <rachel@rachelbaker.me>
2013-01-13 20:36:20 -0600

    Removed extra lines in <head>

--- header.php
+++ header.php
@@ -11,10 +11,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title><?php wp_title('|', true, 'right'); ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
-
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/ico/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144"
           href="<?php echo get_template_directory_uri();?>/assets/ico/apple-touch-icon-144-precomposed.png">
