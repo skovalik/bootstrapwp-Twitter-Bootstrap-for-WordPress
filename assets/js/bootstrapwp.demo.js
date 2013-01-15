01f35886d76ddc2d40071d633449fd9fa721d085
Rachel Baker <rachel@rachelbaker.me>
2013-01-14 22:11:16 -0600

    Updating demo.js from BS 2.3

--- assets/js/bootstrapwp.demo.js
+++ assets/js/bootstrapwp.demo.js
@@ -1,5 +1,5 @@
 /* ===================================================
- * bootstrapwp.demo.js v.91
+ * bootstrapwp.demo.js v.93
  * ===================================================
 
 // NOTICE!! This JS file is included for reference.
@@ -9,60 +9,74 @@
 
 !function ($) {
 
-  $(function(){
-    var $window = $(window);
-    // Adding the needed html to WordPress navigation menus //
-    $("ul.dropdown-menu").parent().addClass("dropdown");
-    $("ul.nav li.dropdown a").addClass("dropdown-toggle");
-    $("ul.dropdown-menu li a").removeClass("dropdown-toggle");
-    $('a.dropdown-toggle').attr('data-toggle', 'dropdown');
+    $(function(){
+        var $window = $(window);
 
-    // Adding dropdown caret for dropdown menus, if it does not already exist
-    $('div.nav-collapse .dropdown-toggle:not(:has(b.caret))').append('<b class="caret"></b>');
-     // side bar
-    $('.bs-docs-sidenav').affix({
-      offset: {
-        top: function () { return $window.width() <= 980 ? 290 : 210 }
-      , bottom: 270
-      }
+        // Adding the needed html to WordPress navigation menus //
+        $("ul.dropdown-menu").parent().addClass("dropdown");
+        $("ul.nav li.dropdown a").addClass("dropdown-toggle");
+        $("ul.dropdown-menu li a").removeClass("dropdown-toggle");
+        $('a.dropdown-toggle').attr('data-toggle', 'dropdown');
+
+        // Adding dropdown caret for dropdown menus, if it does not already exist
+        $('div.nav-collapse .dropdown-toggle:not(:has(b.caret))').append('<b class="caret"></b>');
+
+        // side bar
+        setTimeout(function () {
+          $('.bs-docs-sidenav').affix({
+            offset: {
+              top: function () { return $window.width() <= 980 ? 290 : 210 }
+            , bottom: 270
+            }
+          })
+        }, 100)
     });
 
     // make code pretty
-    window.prettyPrint && prettyPrint();
+    window.prettyPrint && prettyPrint()
 
- // add-ons
+    // add-ons
     $('.add-on :checkbox').on('click', function () {
-      var $this = $(this)
-        , method = $this.attr('checked') ? 'addClass' : 'removeClass'
-      $(this).parents('.add-on')[method]('active')
-    });
+        var $this = $(this)
+            , method = $this.attr('checked') ? 'addClass' : 'removeClass'
+        $(this).parents('.add-on')[method]('active')
+    })
 
     // add tipsies to grid for scaffolding
     if ($('#gridSystem').length) {
-      $('#gridSystem').tooltip({
-          selector: '.show-grid > div'
-        , title: function () { return $(this).width() + 'px' }
-      });
+        $('#gridSystem').tooltip({
+            selector: '.show-grid > div:not(.tooltip)'
+            , title: function () { return $(this).width() + 'px' }
+        })
     }
 
     // tooltip demo
     $('.tooltip-demo').tooltip({
-      selector: "a[rel=tooltip]"
-    });
+        selector: "a[data-toggle=tooltip]"
+    })
 
-    $('.tooltip-test').tooltip();
-    $('.popover-test').popover();
+    $('.tooltip-test').tooltip()
+    $('.popover-test').popover()
 
-        // popover demo
-    $("a[rel=popover]")
-      .popover()
-      .click(function(e) {
-        e.preventDefault()
-      });
+    // popover demo
+    $("a[data-toggle=popover]")
+        .popover()
+        .click(function(e) {
+            e.preventDefault()
+      })
+
+    // button state demo
+    $('#fat-btn')
+        .click(function () {
+            var btn = $(this)
+            btn.button('loading')
+            setTimeout(function () {
+                btn.button('reset')
+            }, 3000)
+    })
 
     // carousel demo
-    $('#myCarousel').carousel();
+    $('#myCarousel').carousel()
 
  });
-
 }(window.jQuery);
