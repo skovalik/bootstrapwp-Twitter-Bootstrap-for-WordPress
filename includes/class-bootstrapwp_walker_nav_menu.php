3ee0389ee1591876dffaf9c37495354ad3277989
Rachel Baker <rachel@rachelbaker.me>
2013-05-20 21:39:27 -0500

    Fixed PHP Debug warning for missing  parameter within Bootstrapwp_Walker_Nav_Menu::start_lvl

--- includes/class-bootstrapwp_walker_nav_menu.php
+++ includes/class-bootstrapwp_walker_nav_menu.php
@@ -11,10 +11,9 @@ class Bootstrapwp_Walker_Nav_Menu extends Walker_Nav_Menu
     {
     }
 
-    function start_lvl(&$output, $depth)
+    function start_lvl(&$output, $depth = 0, $args = array())
     {
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<ul class=\"dropdown-menu\">\n";
     }
-
 }
