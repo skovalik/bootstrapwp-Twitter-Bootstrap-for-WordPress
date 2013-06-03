0b2b2cfd717f6fbfb972559f9b7807eea9fd4671
Rachel Baker <rbaker@nerdery.com>
2013-06-02 20:54:14 -0500

    Updated author.php template file to include conditional display of post-thumbnail.

--- author.php
+++ author.php
@@ -18,47 +18,53 @@ get_header(); ?>
                 bootstrapwp_breadcrumbs();
             } ?>
             </div>
-            <!--/.span12 -->
         </div><!--/.row -->
 
-    <header class="subhead" id="overview">
-        <h1 class="page-title author"><?php printf(
-            __('Author Archives: %s', 'bootstrapwp'),
-            '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url(
-                get_the_author_meta("ID")
-            ) . '" title="' . esc_attr(get_the_author()) . '" rel="me">' . get_the_author() . '</a></span>'
-        ); ?></h1>
-    </header>
-    <?php
-    // Rewind the loop back
-        rewind_posts();
-    ?>
 		<div class="row content">
 			<div class="span8">
-				<?php /* Start the Loop */ ?>
-    <?php while (have_posts()) : the_post(); ?>
-        <div <?php post_class(); ?>>
-            <a href="<?php the_permalink(); ?>" title="<?php the_title();?>"><h3><?php the_title();?></h3></a>
+                <header class="subhead" id="overview">
+                    <h1 class="page-title author"><?php printf(
+                        __('Author Archives: %s', 'bootstrapwp'),
+                        '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url(
+                            get_the_author_meta("ID")
+                        ) . '" title="' . esc_attr(get_the_author()) . '" rel="me">' . get_the_author() . '</a></span>'
+                    ); ?></h1>
+                </header>
+                <?php
+                // Rewind the loop back
+                    rewind_posts();
+                ?>
 
-            <p class="meta"><?php echo bootstrapwp_posted_on();?></p>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div <?php post_class(); ?>>
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title();?>"><h3><?php the_title();?></h3></a>
 
-            <div class="row">
-                <div class="span2">
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                        <?php echo bootstrapwp_autoset_featured_img(); ?></a>
-                </div>
-                <!-- /.span2 -->
-                <div class="span6">
-                    <?php the_excerpt();?>
-                </div>
-                <!-- /.span6 -->
-            </div>
-            <!-- /.row -->
-            <hr/>
-        </div><!-- /.post_class -->
-        <?php endwhile; ?>
-    <?php endif; ?>
-		</div><!-- /.span8 -->
-<?php get_sidebar('blog'); ?>
+                        <p class="meta"><?php echo bootstrapwp_posted_on();?></p>
 
-<?php get_footer(); ?>
+                        <div class="row">
+                            <?php // Post thumbnail conditional display.
+                            if ( bootstrapwp_autoset_featured_img() !== false ) : ?>
+                                <div class="span2">
+                                    <a href="<?php the_permalink(); ?>" title="<?php  the_title_attribute( 'echo=0' ); ?>">
+                                        <?php echo bootstrapwp_autoset_featured_img(); ?>
+                                    </a>
+                                </div>
+                                <div class="span6">
+                            <?php else : ?>
+                                <div class="span8">
+                            <?php endif; ?>
+                                    <?php the_excerpt(); ?>
+                                </div>
+                        </div><!-- /.row -->
+
+                        <hr/>
+                    </div><!-- /.post_class -->
+                   <?php endwhile; ?>
+
+                   <?php bootstrapwp_content_nav('nav-below');?>
+
+               <?php endif; ?>
+		</div>
+
+    <?php get_sidebar('blog'); ?>
+    <?php get_footer(); ?>
