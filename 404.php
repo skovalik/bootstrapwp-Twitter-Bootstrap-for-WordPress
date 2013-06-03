5f9432fa36b9ae2a6a896819e8af41688b2cc260
Rachel Baker <rbaker@nerdery.com>
2013-06-02 20:38:17 -0500

    Removed merge conflict mark-up left within 404.php template file.

--- 404.php
+++ 404.php
@@ -6,78 +6,58 @@
  * @subpackage BootstrapWP
  */
 get_header(); ?>
-<div class="row">
-<<<<<<< HEAD
-    <div class="container">
-        <?php if (function_exists('bootstrapwp_breadcrumbs')) {
-        bootstrapwp_breadcrumbs();
-    } ?>
+<div class="container">
+    <div class="row">
+        <div class="span12">
+            <?php if (function_exists('bootstrapwp_breadcrumbs')) {
+            bootstrapwp_breadcrumbs();
+        } ?>
+        </div>
     </div>
-    <!--/.container -->
-=======
-<div class="span4">
-					<h2><?php _e('All Pages', 'bootstrapwp'); ?></h2>
-					<?php wp_page_menu(); ?>
-</div><!--/.span4 -->
-<div class="span4">
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
-					
-						<h2><?php _e( 'Most Used Categories', 'bootstrapwp' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					
-</div><!--/.span4 -->
->>>>>>> bb63cf2a67b5b6c21b5ae33e0955eec1d5b46ddb
-</div><!--/.row -->
-   <div class="container">
+   <div class="row content">
+       <div class="span8">
 
-       <header class="jumbotron subhead" id="overview">
-           <h1><?php _e('This is Embarrassing', 'bootstrapwp'); ?></h1>
+            <header class="page-title">
+                <h1><?php _e('This is Embarrassing', 'bootstrapwp'); ?></h1>
+            </header>
 
-           <p class="lead"><?php _e(
-               'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.',
-               'bootstrapwp'
-           ); ?></p>
-       </header>
+            <p class="lead"><?php _e(
+                'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.',
+                'bootstrapwp'
+            ); ?></p>
 
-       <div class="row content">
-           <div class="span8">
-
-               <div class="well">
-                   <?php get_search_form(); ?>
-
-               </div>
-               <!--/.well -->
-               <div class="row">
-                   <div class="span4">
-                       <h2>All Pages</h2>
-                       <?php wp_page_menu(); ?>
-                   </div>
-                   <!--/.span4 -->
-                   <div class="span4">
-                       <?php the_widget('WP_Widget_Recent_Posts'); ?>
-
-                       <h2><?php _e('Most Used Categories', 'bootstrapwp'); ?></h2>
-                       <ul>
-                           <?php wp_list_categories(
-                           array(
-                               'orderby' => 'count',
-                               'order' => 'DESC',
-                               'show_count' => 1,
-                               'title_li' => '',
-                               'number' => 10
-                           )
-                       ); ?>
-                       </ul>
-
-                   </div>
-                   <!--/.span4 -->
-               </div>
-               <!--/.row -->
+           <div class="well">
+               <?php get_search_form(); ?>
            </div>
-           <!--/.span8 -->
-           <?php get_sidebar(); ?>
 
-<?php get_footer();
+           <div class="row">
+               <div class="span4">
+                   <h2>All Pages</h2>
+                   <?php wp_page_menu(); ?>
+               </div>
+               <!--/.span4 -->
+               <div class="span4">
+                   <?php the_widget('WP_Widget_Recent_Posts'); ?>
+
+                   <h2><?php _e('Most Used Categories', 'bootstrapwp'); ?></h2>
+                   <ul>
+                       <?php wp_list_categories(
+                       array(
+                           'orderby' => 'count',
+                           'order' => 'DESC',
+                           'show_count' => 1,
+                           'title_li' => '',
+                           'number' => 10
+                       )
+                   ); ?>
+                   </ul>
+
+               </div>
+               <!--/.span4 -->
+           </div>
+           <!--/.row -->
+       </div>
+
+    <?php get_sidebar(); ?>
+    <?php get_footer();
