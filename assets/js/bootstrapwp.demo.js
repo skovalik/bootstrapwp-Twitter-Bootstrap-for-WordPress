0d80887ac8c63b857d7b517096268eed1e668c55
Rachel Baker <rbaker@nerdery.com>
2013-06-02 20:59:25 -0500

    Added function to insert class to 2nd level navigation sub-menus to correct display.

--- assets/js/bootstrapwp.demo.js
+++ assets/js/bootstrapwp.demo.js
@@ -1,5 +1,5 @@
 /* ===================================================
- * bootstrapwp.demo.js v.93
+ * bootstrapwp.demo.js v1.0
  * ===================================================
 
 // NOTICE!! This JS file is included for reference.
@@ -7,76 +7,77 @@
 // demos and examples for BootstrapWP
 // ++++++++++++++++++++++++++++++++++++++++++*/
 
-!function ($) {
+!function($) {
 
     $(function(){
         var $window = $(window);
 
         // Adding the needed html to WordPress navigation menus //
         $("ul.dropdown-menu").parent().addClass("dropdown");
+        $("ul.dropdown-menu ul").parent().addClass("dropdown-submenu");
         $("ul.nav li.dropdown a").addClass("dropdown-toggle");
         $("ul.dropdown-menu li a").removeClass("dropdown-toggle");
         $('a.dropdown-toggle').attr('data-toggle', 'dropdown');
 
-        // Adding dropdown caret for dropdown menus, if it does not already exist
-        $('div.nav-collapse .dropdown-toggle:not(:has(b.caret))').append('<b class="caret"></b>');
-
         // side bar
-        setTimeout(function () {
-          $('.bs-docs-sidenav').affix({
-            offset: {
-              top: function () { return $window.width() <= 980 ? 290 : 210 }
-            , bottom: 270
-            }
-          })
-        }, 100)
-    });
+        setTimeout(function() {
 
-    // make code pretty
-    window.prettyPrint && prettyPrint()
+            $('.bs-docs-sidenav').affix({
+                offset: {
+                    top: function () { return $window.width() <= 980 ? 290 : 210 }
+                    , bottom: 270
+                }
+            });
 
-    // add-ons
-    $('.add-on :checkbox').on('click', function () {
-        var $this = $(this)
+            // Adding dropdown caret for dropdown menus, if it does not already exist
+            $('.dropdown-toggle:not(:has(b.caret))').append('<b class="caret"></b>');
+
+        }, 300);
+
+        // add-ons
+        $('.add-on :checkbox').on('click', function () {
+          var $this = $(this)
             , method = $this.attr('checked') ? 'addClass' : 'removeClass'
-        $(this).parents('.add-on')[method]('active')
-    })
+          $(this).parents('.add-on')[method]('active')
+        });
 
-    // add tipsies to grid for scaffolding
-    if ($('#gridSystem').length) {
-        $('#gridSystem').tooltip({
-            selector: '.show-grid > div:not(.tooltip)'
+        // add tipsies to grid for scaffolding
+        if ($('#gridSystem').length) {
+          $('#gridSystem').tooltip({
+              selector: '.show-grid > [class*="span"]'
             , title: function () { return $(this).width() + 'px' }
-        })
-    }
+          })
+        };
 
-    // tooltip demo
-    $('.tooltip-demo').tooltip({
-        selector: "a[data-toggle=tooltip]"
-    })
+        // tooltip demo
+        $('.tooltip-demo').tooltip({
+          selector: "a[data-toggle=tooltip]"
+        });
 
-    $('.tooltip-test').tooltip()
-    $('.popover-test').popover()
+        $('.tooltip-test').tooltip();
+        $('.popover-test').popover();
 
-    // popover demo
-    $("a[data-toggle=popover]")
-        .popover()
-        .click(function(e) {
-            e.preventDefault()
-      })
+        // popover demo
+        $("a[data-toggle=popover]")
+          .popover()
+          .click(function(e) {
+            e.preventDefault();
+          });
 
-    // button state demo
-    $('#fat-btn')
-        .click(function () {
-            var btn = $(this)
-            btn.button('loading')
+        // button state demo
+        $('#fat-btn')
+          .click(function () {
+            var btn = $(this);
+            btn.button('loading');
+
             setTimeout(function () {
-                btn.button('reset')
-            }, 3000)
-    })
+              btn.button('reset');
+            }, 3000);
 
-    // carousel demo
-    $('#myCarousel').carousel()
+          });
+
+        // carousel demo
+        $('#myCarousel').carousel();
 
  });
 }(window.jQuery);
